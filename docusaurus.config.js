17974e2560eebc79a178234753f6971a8395bba4
Maxime Goossens <maxime.goossens@gmail.com>
2021-11-17 19:19:46 +0000

    Update documentation urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,13 +24,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/hotspotty/documentation',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+            'https://github.com/hotspotty/documentation/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
